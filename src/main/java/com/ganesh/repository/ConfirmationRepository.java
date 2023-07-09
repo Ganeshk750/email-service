2543a6c3ad20540640f107b1e2731bbd6f5c8280
GANESHKUMAR <ganesh9ot@gmail.com>
2023-07-09 12:13:14 +0530

    @ changing import

--- src/main/java/com/ganesh/repository/ConfirmationRepository.java
+++ src/main/java/com/ganesh/repository/ConfirmationRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConfirmationRepository extends JpaRepository<ConfirmationRepository, Long> {
+public interface ConfirmationRepository extends JpaRepository<Confirmation, Long> {
 
     Confirmation findByToken(String token);
 }
