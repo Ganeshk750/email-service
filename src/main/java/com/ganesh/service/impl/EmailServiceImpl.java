37d233db1b57dd96cebdc961be8e150ac4f4b11d
GANESHKUMAR <ganesh9ot@gmail.com>
2023-07-17 19:40:44 +0530

    # adding EmailService with Impl class

---
+++ src/main/java/com/ganesh/service/impl/EmailServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ganesh.service.impl;
+
+import com.ganesh.service.EmailService;
+
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void simpleMailMessage(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendMimeMessageWithAttachments(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendMimeMessageWithEmbeddedImages(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendMimeMessageWithEmbeddedFiles(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendHtmlEmail(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendHtmlEmailWithEmbeddedFiles(String name, String to, String token) {
+
+    }
+}
