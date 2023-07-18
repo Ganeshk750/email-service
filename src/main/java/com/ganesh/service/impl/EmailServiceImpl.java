4bccae6bad0f7e079210a2df59243d2583be9814
GANESHKUMAR <ganesh9ot@gmail.com>
2023-07-18 22:37:03 +0530

    # adding SimpleMailMessage Implementation

--- src/main/java/com/ganesh/service/impl/EmailServiceImpl.java
+++ src/main/java/com/ganesh/service/impl/EmailServiceImpl.java
@@ -1,12 +1,37 @@
 package com.ganesh.service.impl;
 
 import com.ganesh.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    @Value("${spring.mail.verify.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    private final JavaMailSender emailSender;
 
     @Override
     public void simpleMailMessage(String name, String to, String token) {
-
+       try {
+           SimpleMailMessage message = new SimpleMailMessage();
+           message.setSubject("New User Account Verification");
+           message.setFrom(fromEmail);
+           message.setTo(to);
+           message.setText("Hey This is SimpleMailMessage Testing.");
+           emailSender.send(message);
+       }catch (Exception exception){
+           System.out.println(exception.getMessage());
+           throw new RuntimeException(exception.getMessage());
+       }
     }
 
     @Override
