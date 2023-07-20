bb6e7c626b35dc82fd929b8a63dd380f0e5bbf71
GANESHKUMAR <ganesh9ot@gmail.com>
2023-07-20 22:06:38 +0530

    # Enable Async communication for entire application   using @EnableAsync

--- src/main/java/com/ganesh/EmailServiceApplication.java
+++ src/main/java/com/ganesh/EmailServiceApplication.java
@@ -2,8 +2,10 @@ package com.ganesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
