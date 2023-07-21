25f67604db9504a93ead735ad2d9082a6a17e318
GANESHKUMAR <ganesh9ot@gmail.com>
2023-07-21 22:21:54 +0530

    # implementing sendMimeMessageWithAttachments

--- src/main/java/com/ganesh/service/impl/EmailServiceImpl.java
+++ src/main/java/com/ganesh/service/impl/EmailServiceImpl.java
@@ -2,17 +2,23 @@ package com.ganesh.service.impl;
 
 import com.ganesh.service.EmailService;
 import com.ganesh.utils.EmailUtils;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
+    public static final String UTF_8_ENCODING = "UTF-8";
     @Value("${spring.mail.verify.host}")
     private String host;
 
@@ -36,12 +42,32 @@ public class EmailServiceImpl implements EmailService {
            System.out.println(exception.getMessage());
            throw new RuntimeException(exception.getMessage());
        }
+
     }
 
     @Override
     @Async
     public void sendMimeMessageWithAttachments(String name, String to, String token) {
-
+       try {
+           MimeMessage message = getMimeMessage();
+           MimeMessageHelper helper = new MimeMessageHelper(message, true,UTF_8_ENCODING);
+           helper.setPriority(1);
+           helper.setSubject("New User Account Verification");
+           helper.setFrom(fromEmail);
+           helper.setTo(to);
+           helper.setText(EmailUtils.getEmailMessage(name, host,token));
+           // Add Attachment form file system
+           FileSystemResource first = new FileSystemResource(new File(System.getProperty("user.home") + "/Downloads/images/first.png"));
+           FileSystemResource second = new FileSystemResource(new File(System.getProperty("user.home") + "/Downloads/images/second.png"));
+           FileSystemResource third = new FileSystemResource(new File(System.getProperty("user.home") + "/Downloads/images/third.rtf"));
+           helper.addAttachment(first.getFilename(), first);
+           helper.addAttachment(second.getFilename(), second);
+           helper.addAttachment(third.getFilename(), third);
+           emailSender.send(message);
+       }catch (Exception exception){
+           System.out.println(exception.getMessage());
+           throw new RuntimeException(exception.getMessage());
+       }
     }
 
     @Override
@@ -66,5 +92,9 @@ public class EmailServiceImpl implements EmailService {
     @Async
     public void sendHtmlEmailWithEmbeddedFiles(String name, String to, String token) {
 
+    }
+
+    private MimeMessage getMimeMessage() {
+        return emailSender.createMimeMessage();
     }
 }
